c8c1e4d4b6782b7b931d10526b297ffe21911853
standaloneee <tzherzdev@gmail.com>
2022-11-18 22:21:31 +0300

    Initial commit

---
+++ src/test/java/ru/cocktails/CocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cocktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
